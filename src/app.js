f8fcd4a500332c24ee4474cb9b952df17af50943
Pharaoh <pharaohmanson@gmail.com>
2025-05-07 12:29:37 -0400

    cors

--- src/app.js
+++ src/app.js
@@ -22,7 +22,7 @@ app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
 
-app.use(cors({ origin: [ 'http://localhost:3000', 'https://loca-sphere.vercel.app/'], credentials: true }));
+app.use(cors({ origin: [ 'http://localhost:3000', 'https://loca-sphere.vercel.app'], credentials: true }));
 
 app.use('/api', apiRouter);
 
